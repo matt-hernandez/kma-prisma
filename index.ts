1b6a556006e00826b2cb7ba84293d4c69e2df512
Matt Hernandez <matt.hernandez@capgemini.com>
2019-09-12 17:06:20 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { prisma } from './generated/prisma-client'
+
+// A `main` function so that we can use async/await
+async function main() {
+  // Create a new user called `Alice`
+  const newUser = await prisma.createUser({
+    name: 'Matt Hernandez',
+    email: 'matt.hernandez@gmail.com',
+    isAdmin: true,
+    loginTimestamp: Date.now() * 1000
+  })
+  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+
+  // Read all users from the database and print them to the console
+  const allUsers = await prisma.users()
+  console.log(allUsers)
+}
+
+main().catch(e => console.error(e))
