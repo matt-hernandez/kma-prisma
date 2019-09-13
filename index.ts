369cc227487f6f6fb5e975dce62179326f821223
Matt Hernandez <matt.hernandez@capgemini.com>
2019-09-13 11:27:19 -0500

    Final touchups on datamodel

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import { prisma } from './generated/prisma-client'
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user called `Alice`
   const newUser = await prisma.createUser({
     name: 'Matt Hernandez',
     email: 'matt.hernandez@gmail.com',
