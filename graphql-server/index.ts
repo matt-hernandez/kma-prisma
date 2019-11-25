ed93ec591530c4a0824e79e6447f572a7beab1d8
Matt Hernandez <matt.hernandez@capgemini.com>
2019-11-25 16:22:32 -0600

    Clean up unused variables

--- graphql-server/index.ts
+++ graphql-server/index.ts
@@ -1,13 +1,13 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { mergeTypes } from 'merge-graphql-schemas';
-import { shield, and, chain } from 'graphql-shield';
+import { shield, chain } from 'graphql-shield';
 import { userQueryResolvers, userQuerySchema } from './user/query';
 import { userMutationResolvers, userMutationSchema } from './user/mutation';
 import { adminQueryResolvers, adminQuerySchema } from './admin/query';
 import { adminMutationResolvers, adminMutationSchema } from './admin/mutation';
 import { isAuthenticated, isAdmin } from './utilities/shield-rules';
-import Resolvers, { ResolverFunction, Resolver } from './utilities/resolvers-type';
+import Resolvers, { ResolverFunction } from './utilities/resolvers-type';
 import { Rule } from 'graphql-shield/dist/rules';
 
 export const types = readFileSync(resolve(__dirname, 'types.graphql'), 'utf8');
