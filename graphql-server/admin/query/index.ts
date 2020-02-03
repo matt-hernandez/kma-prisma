37849b2c513204a8327b9df5a30348fbc9a8c7bd
Matt Hernandez <matt.isaiah.hernandez@gmail.com>
2020-02-03 12:13:13 -0600

    Add safeguards to filter query results that include the user themselves

--- graphql-server/admin/query/index.ts
+++ graphql-server/admin/query/index.ts
@@ -7,7 +7,10 @@ import { TODAY_MILLISECONDS } from '../../utilities/date';
 export const adminQuerySchema = readFileSync(resolve(__dirname, 'query.graphql'), 'utf8');
 
 export const adminQueryResolvers: Resolvers = {
-  users: (root, args, { prisma }) => prisma.users(),
+  users: async (root, args, { user, prisma }) => {
+    const users = await prisma.users();
+    return users.filter(({ cid }) => user.cid !== cid);
+  },
   currentTasks: async (root, args, { user, prisma }) => {
     let tasks = await prisma.tasks();
     const utcTime = TODAY_MILLISECONDS;
@@ -36,7 +39,10 @@ export const adminQueryResolvers: Resolvers = {
     return tasks.map(task => adminTaskPipe(task, prisma));
   },
   taskTemplates: (root, args, { prisma }) => prisma.taskTemplates(),
-  claims: (root, args, { prisma }) => prisma.outcomes({ where: { type: 'PENDING' } }),
+  claims: async (root, args, { user, prisma }) => {
+    const outcomes = await prisma.outcomes({ where: { type: 'PENDING' } });
+    return outcomes.filter(({ userCid }) => user.cid !== userCid);
+  },
   userScore: async (root, { cid }, { prisma }) => {
     const user = await prisma.user({ cid });
     return userScorePipe(user, prisma);
